2ef657417e730dbe29d9415217b4329b1e7a0fb8
kylesloper <45979758+kylesloper@users.noreply.github.com>
2025-07-11 02:13:06 +0100

    Create index.ts

---
+++ library/index.ts
@@ -0,0 +1,29 @@
+import { Component } from "https://cdn.jsdelivr.net/gh/kylesloper/lume-cms@8660f6622a67a34000b696cbee165e9772603da8/static/components/component.js"
+import { push } from "https://cdn.jsdelivr.net/gh/kylesloper/lume-cms@8660f6622a67a34000b696cbee165e9772603da8/static/components/utils.js"
+
+customElements.define(
+    "library-field",
+    class extends Component {
+        init() {
+            // Get the field info
+            const { schema, value, namePrefix } = this
+
+            // Generate the name of the input and the id
+            const name = `${namePrefix}.${schema.name}`
+            const id = `field_${name}`
+
+            // Add the <label>
+            push(this, "label", { for: id }, schema.label)
+
+            // Add the <input type="number">
+            push(this, "input", {
+                name,
+                value,
+                id,
+                type: "number",
+                min: 0,
+                max: 100,
+            })
+        }
+    }
+)
